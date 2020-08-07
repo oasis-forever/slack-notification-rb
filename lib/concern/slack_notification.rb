b47a5c0a17a42d86d5733a030a7fc0b573d2406a
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2020-08-07 23:11:13 +0900

    Fix module call

--- lib/concern/slack_notification.rb
+++ lib/concern/slack_notification.rb
@@ -1,11 +1,11 @@
 require 'slack/incoming/webhooks'
 
-class SlackNotifier
+module SlackNotifier
   def self.post(msg)
     # Webhook URLは .zzhrc / .bash_profile に環境変数として設定
     slack = Slack::Incoming::Webhooks.new(ENV['MY_CHANNEL'])
     # アイコンの絵文字設定
-    slack.icon_emoji = ':smile:'
+    slack.icon_emoji = ':ishida:'
     # インスタンスメソッドとして呼出し元でメッセージを渡す
     slack.post(msg)
   end
--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,4 @@
-require './lib/slack_notification.rb'
+require './concern/slack_notification.rb'
 
 class User
   attr_accessor :name, :age
@@ -7,11 +7,7 @@ class User
     @name = name
     @age = age
   end
-
-  def post_to_slack
-    SlackNotifier.post("Hi, my name is #{name} and I'm #{age} years old.")
-  end
 end
 
 user = User.new(name: 'Hayato Ishida', age: 29)
-user.post_to_slcak
+SlackNotifier.post("Hi, my name is #{user.name} and I'm #{user.age} years old.")
